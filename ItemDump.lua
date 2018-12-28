0d8e827e76056c6a4e78eb0286795a841fdf2d3d
Jnnshschl <jannis@jnns.de>
2018-12-28 14:37:05 +0100

    Spellbook dump

--- ItemDump.lua
+++ ItemDump.lua
@@ -2,7 +2,7 @@
 -- to check if we could need that item
 
 -- v change that lmao
-abotItemName = "";
+abotItemName = "Butcher's Slicer";
 
 abotItemInfoResult='noItem';
 abName,abLink,abRarity,abLevel,abMinLevel,abType,abSubType,abStackCount,abEquipLoc,abIcon,abSellPrice=GetItemInfo(abotItemName);
@@ -26,3 +26,9 @@ abotItemInfoResult=
     '"equiplocation": "'..tostring(abEquipLoc or 0)..'",'..
     '"sellprice": "'..tostring(abSellPrice or 0)..'"'..
 '}';
+
+abotItemName="Forest Leather Gloves";
+abotItemInfoResult='noItem';
+abName,abLink,abRarity,abLevel,abMinLevel,abType,abSubType,abStackCount,abEquipLoc,abIcon,abSellPrice=GetItemInfo(abotItemName);
+print(abLink);
+abotItemInfoResult='{'..'"id": "0",'..'"count": "1",'..'"quality": "'..tostring(abRarity or 0)..'",'..'"curDurability": "0",'..'"maxDurability": "0",'..'"cooldownStart": "0",'..'"cooldownEnd": "0",'..'"name": "'..tostring(abName or 0)..'",'..'"link": "'..tostring(abLink or 0)..'",'..'"level": "'..tostring(abLevel or 0)..'",'..'"minLevel": "'..tostring(abMinLevel or 0)..'",'..'"type": "'..tostring(abType or 0)..'",'..'"subtype": "'..tostring(abSubType or 0)..'",'..'"maxStack": "'..tostring(abStackCount or 0)..'",'..'"equiplocation": "'..tostring(abEquipLoc or 0)..'",'..'"sellprice": "'..tostring(abSellPrice or 0)..'"'..'}';
