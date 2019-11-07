e00fb03240ab988ee80a719aef4fc59e5d3ff7f0
Jannis Höschele <jannis@jnns.de>
2019-11-07 17:31:54 +0100

    cleanup, item stats, readme

--- InventoryDump.lua
+++ InventoryDump.lua
@@ -1,47 +1,58 @@
--- Will dump all items of your bags
+-- will dump all your inventory items
 
-abotInventoryResult='[';
-for b=0,4 do
-	containerSlots=GetContainerNumSlots(b);
+abotInventoryResult = "["
 
-	for s=1,containerSlots do
-		abItemLink=GetContainerItemLink(b,s)
+for a = 0, 4 do
+    containerSlots = GetContainerNumSlots(a)
 
-		if abItemLink then
-			abCurrentDurability, abMaxDurability = GetContainerItemDurability(b, s);
-			abCooldownStart, abCooldownEnd = GetContainerItemCooldown(b, s)
-			abIcon, abItemCount, abLocked, abQuality, abReadable, abLootable, abItemLink, isFiltered = GetContainerItemInfo(b, s);
-			abName,abLink,abRarity,abLevel,abMinLevel,abType,abSubType,abStackCount,abEquipLoc,abIcon,abSellPrice=GetItemInfo(abItemLink);
+    for b = 1, containerSlots do
+        abId = GetContainerItemID(a, b)
 
-			abotInventoryResult=abotInventoryResult..
-				'{'..
-					'"id": "'..tostring(abId or 0)..'",'..
-					'"count": "'..tostring(abItemCount or 0)..'",'..
-					'"quality": "'..tostring(abQuality or 0)..'",'..
-					'"curDurability": "'..tostring(abCurrentDurability or 0)..'",'..
-					'"maxDurability": "'..tostring(abMaxDurability or 0)..'",'..
-					'"cooldownStart": "'..tostring(abCooldownStart or 0)..'",'..
-					'"cooldownEnd": "'..tostring(abCooldownEnd or 0)..'",'..
-					'"name": "'..tostring(abName or 0)..'",'..
-					'"lootable": "'..tostring(abLootable or 0)..'",'..
-					'"readable": "'..tostring(abReadable or 0)..'",'..
-					'"link": "'..tostring(abItemLink or 0)..'",'..
-					'"level": "'..tostring(abLevel or 0)..'",'..
-					'"minLevel": "'..tostring(abMinLevel or 0)..'",'..
-					'"type": "'..tostring(abType or 0)..'",'..
-					'"subtype": "'..tostring(abSubType or 0)..'",'..
-					'"maxStack": "'..tostring(abStackCount or 0)..'",'..
-					'"equiplocation": "'..tostring(abEquipLoc or 0)..'",'..
-					'"sellprice": "'..tostring(abSellPrice or 0)..'"'..
-					'"bagid": "'..tostring(b or 0)..'"'..
-					'"bagslot": "'..tostring(s or 0)..'"'..
-				'}';
+        if (string.len(tostring(abId or "")) > 0) then
+            abItemLink = GetContainerItemLink(a, b)
+            abCurrentDurability, abMaxDurability = GetContainerItemDurability(a, b)
+            abCooldownStart, abCooldownEnd = GetContainerItemCooldown(a, b)
+            abIcon, abItemCount, abLocked, abQuality, abReadable, abLootable, abItemLink, isFiltered = GetContainerItemInfo(a, b)
+            abName, abLink, abRarity, abLevel, abMinLevel, abType, abSubType, abStackCount, abEquipLoc, abIcon, abSellPrice = GetItemInfo(abItemLink)
 
-			if s < containerSlots then
-				abotInventoryResult=abotInventoryResult..',';
-			end
-		end
-	end
+            stats={};
+            abStats=GetItemStats(abotItemLink, stats);
+    
+            statsResult={};
+            for key, value in pairs(stats) do
+                table.insert(statsResult, string.format("\"%s\":\"%s\"", key, value))
+            end
+
+            abotInventoryResult = abotInventoryResult..
+                "{"..
+                    '"id": "'..tostring(abId or 0)..'",'..
+                    '"count": "'..tostring(abItemCount or 0)..'",'..
+                    '"quality": "'..tostring(abQuality or 0)..'",'..
+                    '"curDurability": "'..tostring(abCurrentDurability or 0)..'",'..
+                    '"maxDurability": "'..tostring(abMaxDurability or 0)..'",'..
+                    '"cooldownStart": "'..tostring(abCooldownStart or 0)..'",'..
+                    '"cooldownEnd": "'..tostring(abCooldownEnd or 0)..'",'..
+                    '"name": "'..tostring(abName or 0)..'",'..
+                    '"lootable": "'..tostring(abLootable or 0)..'",'..
+                    '"readable": "'..tostring(abReadable or 0)..'",'..
+                    '"link": "'..tostring(abItemLink or 0)..'",'..
+                    '"level": "'..tostring(abLevel or 0)..'",'..
+                    '"minLevel": "'..tostring(abMinLevel or 0)..'",'..
+                    '"type": "'..tostring(abType or 0)..'",'..
+                    '"subtype": "'..tostring(abSubType or 0)..'",'..
+                    '"maxStack": "'..tostring(abStackCount or 0)..'",'..
+                    '"equiplocation": "'..tostring(abEquipLoc or 0)..'",'..
+                    '"sellprice": "'..tostring(abSellPrice or 0)..'",'..
+                    '"stats": '.."{" .. table.concat(statsResult, ",").."}"..','..
+                    '"bagid": "'..tostring(a or 0)..'",'..
+                    '"bagslot": "'..tostring(b or 0)..'"'
+                .."}"
+
+            if b < containerSlots then
+                abotInventoryResult = abotInventoryResult .. ","
+            end
+        end
+    end
 end
 
-abotInventoryResult=abotInventoryResult..']';
+abotInventoryResult = abotInventoryResult .. "]"
--- README.md
+++ README.md
@@ -1,44 +1,58 @@
-# Collection of Lua stuff for WoW
+# Collection of Lua scripts for WoW
+In this repo you can find some Lua scripts to dump stuff from WoW to a ```json``` string.
+These script are used in my bot to extract various things from the game via its Lua Api.
 
-In this repo you can find some Lua scripts to dump stuff from WoW to a JSON.
-
-These script are used in my bot.
+## EquipmentDump.lua
+Dumps all items that you have equipped as ```json```
 
 ## EquipmentSlotDump.lua
-will dump any InventorySlot as json
+Dumps a item that you have equipped as ```json```
 
-```c#
-public enum InventorySlot : int
-    {
-        INVSLOT_AMMO = 0,
-        INVSLOT_HEAD = 1,
-        INVSLOT_NECK = 2,
-        INVSLOT_SHOULDER = 3,
-        INVSLOT_SHIRT = 4,
-        INVSLOT_CHEST = 5,
-        INVSLOT_WAIST = 6,
-        INVSLOT_LEGS = 7,
-        INVSLOT_FEET = 8,
-        INVSLOT_WRIST = 9,
-        INVSLOT_HANDS = 10,
-        INVSLOT_RING1 = 11,
-        INVSLOT_RING2 = 12,
-        INVSLOT_TRINKET1 = 13,
-        INVSLOT_TRINKET2 = 14,
-        INVSLOT_BACK = 15,
-        INVSLOT_MAINHAND = 16,
-        INVSLOT_OFFHAND = 17,
-        INVSLOT_RANGED = 18,
-        INVSLOT_TABARD = 19,
-        CONTAINER_BAG_1 = 20,
-        CONTAINER_BAG_2 = 21,
-        CONTAINER_BAG_3 = 22,
-        CONTAINER_BAG_4 = 23
-    }
-```
+## EventCapture.lua
+Dumps collected events as ```json```
 
 ## InventoryDump.lua
-will dump you whole bag items to a json
+Dumps all items in your bag as ```json```
+
+## ItemDump.lua
+Dumps a specific item as ```json```
 
 ## ItemStatsDump.lua
-will dump the primary stats from an item to a json
+Dumps the Primary stats of a specific item as ```json```
+
+## SkillDump.lua
+Dumps all skills as ```json```
+
+## SpellbookDump.lua
+Dumps all spells as ```json```
+
+## Misc stuff
+```c#
+public enum EquipmentSlot : int
+{
+    INVSLOT_AMMO = 0,
+    INVSLOT_HEAD = 1,
+    INVSLOT_NECK = 2,
+    INVSLOT_SHOULDER = 3,
+    INVSLOT_SHIRT = 4,
+    INVSLOT_CHEST = 5,
+    INVSLOT_WAIST = 6,
+    INVSLOT_LEGS = 7,
+    INVSLOT_FEET = 8,
+    INVSLOT_WRIST = 9,
+    INVSLOT_HANDS = 10,
+    INVSLOT_RING1 = 11,
+    INVSLOT_RING2 = 12,
+    INVSLOT_TRINKET1 = 13,
+    INVSLOT_TRINKET2 = 14,
+    INVSLOT_BACK = 15,
+    INVSLOT_MAINHAND = 16,
+    INVSLOT_OFFHAND = 17,
+    INVSLOT_RANGED = 18,
+    INVSLOT_TABARD = 19,
+    CONTAINER_BAG_1 = 20,
+    CONTAINER_BAG_2 = 21,
+    CONTAINER_BAG_3 = 22,
+    CONTAINER_BAG_4 = 23
+}
+```
